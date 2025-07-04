4e57f6492b795909f5c285c45f90782c01cf5c57
Pasham Arun <pashamarun@Pashams-MacBook-Air.local>
2025-07-04 10:48:10 +0530

    Initial commit

---
+++ identity.py
@@ -0,0 +1,29 @@
+ #identity operators
+l=[1,2,3,4]
+b=[1,2,3,4]
+print("l is b",l is b)
+a=b
+print("a is b",a is b)
+print("id(1)",id(1))
+print("id(2)",id(2))
+print("a  not in l",a  not in l)
+print("i  not in b",l  not in b)
+
+'3-011'
+'5-101'
+print("3&5:",3&5)
+'4-100'
+'5-101'
+print("4/5:",4/5)
+'5^6=011=3'
+print("5^6:",5^6)
+'1-001'
+print("~1:",~1)
+'2<<1'
+print("2<<1",2<<1)
+'4>>2'
+print("4>>2",4>>2)      
+       
+
+       
+       
---
+++ keywords.py
@@ -0,0 +1,3 @@
+import keyword as kw
+print(keyword.kwlist)
+
